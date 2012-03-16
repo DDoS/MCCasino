0a8635baa812b188ec6725b673b8a49386dcd1d1
DDoSQc <QcTechs@gmail.com>
2012-03-16 18:35:44 -0400

    Added a missing return statement and modified the formatting. Seems the odds are fine.

--- src/me/DDoS/MCCasino/slotmachine/Reel.java
+++ src/me/DDoS/MCCasino/slotmachine/Reel.java
@@ -1,41 +1,52 @@
 package me.DDoS.MCCasino.slotmachine;
 
 import java.security.SecureRandom;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
 import org.bukkit.inventory.ItemStack;
+
 /**
  *
  * @author DDos
  */
 public class Reel {
-    
+
     private static final Random random = new SecureRandom();
     //
     private final List<ReelValue> values;
-    
+
     public Reel(List<ReelValue> values) {
-        
+
         this.values = values;
-        
+
     }
-    
+
     public ItemStack getRandomItem() {
-        // CLEANED THIS FUNCTION & IMPROVED ODDS CALCULATION
+        
         int totalWeights = 0;
-        
+
         for (ReelValue value : values) {
-            totalWeights+= value.getProb();
-        }
+          
+            totalWeights += value.getProb();
         
+        }
+
         int i = 0;
         final int rand = random.nextInt(totalWeights);
-        for (ReelValue values : values) {
+        
+        for (ReelValue value : values) {
+            
             i += value.getProb();
-            if (rand < i)
+            
+            if (rand < i) {
+                
                 return value.getItem();
-        }        
+            
+            }
+        }
+
+        return null;
+
     }
 }
